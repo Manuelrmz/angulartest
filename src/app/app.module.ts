8dabd0806bdfdb6f1182f134562e9dce130141ab
Manuel Ramirez <manuel.ramirez89@gmail.com>
2018-02-02 18:56:28 -0600

    Starting repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { AppRoutingModule }        from './app.routing';
 
+import { AuthGuard } from './_guards/auth.guard';
+import { AuthenticationService,} from './_services/authentication.service';
+import { UserService} from './_services/user.service';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { GestionActivosModule } from './gestion-activos/gestion-activos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        GestionActivosModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent
+    ],
+    providers: [
+        AuthGuard,
+        AuthenticationService,
+        UserService,
+
+        // providers used to create fake backend
+        //fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
